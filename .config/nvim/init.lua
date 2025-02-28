b41a3b089905fef1f895d63bf0948cfda0aeb300
arnnvv <arnav.900@protonmail.com>
2025-02-28 22:02:21 +0530

    updt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,33 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 vim.g.netrw_banner = 0
 
+vim.cmd.colorscheme('habamax')
+
+vim.api.nvim_set_hl(0, 'Normal', {
+    bg = 'none',
+})
+vim.api.nvim_set_hl(0, 'NormalFloat', {
+    bg = 'none',
+})
+
+vim.opt.shell = '/bin/dash'
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.scrolloff = 10
+vim.opt.mouse = ''
+vim.opt.guicursor = ''
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.o.undofile = true
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  once = true,
+  callback = function()
+    require('autopair').setup_autopairs()
+  end,
+})
+
 require('lazy').setup({
   spec = {
     {
@@ -36,5 +63,4 @@ require('lazy').setup({
   },
 })
 
-require('options')
 require('keymaps')
